1f5b588946533a2441e91370f2dd4da68b15e72e
Dawid Urbaniak <udawid00@gmail.com>
2024-01-07 23:09:47 +0100

    install and add graphql

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 
 // import "./header.module.scss";
 import * as headerStyles from "./header.module.scss";
 
 const Header = () => {
+  const data = useStaticQuery();
   return (
     <header className={headerStyles.header}>
       <h1>

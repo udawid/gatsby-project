1d4885369faa2853c4f8cb525c7b74a49eb0282f
Dawid Urbaniak <udawid00@gmail.com>
2024-01-09 10:55:19 +0100

    Rendering Post Data in Blog Template

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,44 @@
 import React from "react";
+import { Link, graphql, useStaticQuery } from "gatsby";
 
 import Layout from "../components/layout";
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              data
+            }
+            html
+            excerpt
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge) => {
+          return (
+            <li>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.data}</p>
+              </Link>
+            </li>
+          );
+        })}
+      </ol>
     </Layout>
   );
 };

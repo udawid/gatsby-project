9e2aea9cf064b56fdb7dfdd417a8750bba331977
Dawid Urbaniak <udawid00@gmail.com>
2024-01-09 13:32:44 +0100

    Add plugin Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
     author: "Dawid Urbaniak",
   },
   plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-sass`,
     {
       resolve: "gatsby-source-filesystem",
@@ -22,6 +29,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
     {
       resolve: "gatsby-transformer-remark",
       options: {
